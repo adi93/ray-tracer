59f7a46bac4839ec96ebf18e32c9f0ca62a07567
Aditya Harit <harit.aditya@gmail.com>
2018-12-18 08:02:05 +0530

    temp

--- main.go
+++ main.go
@@ -10,10 +10,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 )
@@ -24,37 +22,47 @@ import (
 )
 
 var _ = vector.New()
+var _ = ray.New()
+
+const (
+	nx       = 200
+	ny       = 100
+	testFile = "test.ppm"
+)
 
 func main() {
-	ray := ray.NewRay(vector.NewPos3VectorFromValues(0, 0, 0), vector.NewPos3VectorFromValues(1, 0, 0))
-	temp := ray.PointAtParameter(2)
-	fmt.Printf("%v\n", temp)
-	fmt.Printf("%v\n", reflect.TypeOf(temp))
-}
-
-func main2() {
-	nx, ny := 200, 100
 	log.Printf("%d %d", nx, ny)
-	file, err := os.OpenFile("test.ppm", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Error %v", err)
-		os.Exit(1)
-	}
+
+	file := getFile()
 	defer file.Close()
+
 	var str strings.Builder
 	str.WriteString("P3\n" + strconv.Itoa(nx) + " " + strconv.Itoa(ny) + "\n255\n")
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
 			col := vector.NewWithValues(float64(i)/float64(nx), float64(j)/float64(ny), 0.2)
 			ir := int(255.99 * col.Get(0))
-			ig := int(255.99 * col.Get(0))
-			ib := int(255.99 * col.Get(0))
+			ig := int(255.99 * col.Get(1))
+			ib := int(255.99 * col.Get(2))
 			str.WriteString(strconv.Itoa(ir) + " " + strconv.Itoa(ig) + " " + strconv.Itoa(ib) + "\n")
 		}
 	}
-	_, err = file.WriteString(str.String())
+	_, err := file.WriteString(str.String())
+	checkError(err)
+}
+
+func getFile() *os.File {
+	file, err := os.OpenFile(testFile, os.O_APPEND|os.O_WRONLY, 0644)
+	checkError(err)
+	return file
+}
+
+func simpleGradient() {
+}
+
+func checkError(err error) {
 	if err != nil {
-		fmt.Println("Could not write to temp file")
-		log.Fatal(err)
+		log.Fatalf("Error %v", err)
+		os.Exit(1)
 	}
 }
